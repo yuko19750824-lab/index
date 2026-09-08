1eb3a55d4fc71f6dd392ca5272cc90f2f988fb39
yuko19750824-lab <yuko19750824@gmail.com>
2026-09-08 21:01:52 +0900

    Add files via upload

---
+++ index/sample.py
@@ -0,0 +1,262 @@
+<HTML>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="keywords" content="北海道,観光,イベント,旅行,ふれあい,飲食">
+
+<!-- OGP START -->
+<meta property="og:title" content="北海道の観光・イベント・旅行・・・時々、ふれあい">
+<meta property="og:type" content="Website">
+<meta property="og:image" content="">
+<meta property="og:url" content="">
+<meta property="og:site_name" content="北海道の観光・イベント・旅行・・・時々、ふれあい">
+<meta property="og:description" content="北海道の色々な場所を紹介するページ">
+<meta property="og:locale" content="ja_JP">
+<!-- OGP END -->
+
+<title>北海道の観光・イベント・旅行・・・時々、ふれあい | Index</title>
+
+<!--共通CSS-->
+<link rel="stylesheet" href="/common/css/reset.css">
+<link rel="stylesheet" href="/common/css/common.css">
+
+<!--ページ別CSS-->
+<link rel="stylesheet" href="index/css/index.css">
+
+<!--共通JS--> 
+<script src="/common/js/jquery-1.12.3.min.js"></script> 
+<script src="/common/js/common.js"></script> 
+<script type="text/javascript" src="//typesquare.com/3/tsst/script/ja/typesquare.js?64f4881f36104dba9873463dac1e02e5" charset="utf-8"></script>
+<script type="text/javascript" src="//typesquare.com/3/tsst/script/ja/typesquare.js?684d768bae1045b4aedd2e82ac1e02e5" charset="utf-8"></script>
+
+<!--個別JS--> 
+<script src="/index/js/index.js"></script> 
+</head>
+
+<body>
+<div class='container content'>
+
+	<!-- wrap START -->
+	<header>
+		<div class="カテゴリー">
+			<h1>観光</h1>
+			<h2>イベント</h2>
+            <h3>旅行</h3>
+            <h4>ふれあい</h4>
+			<h5>飲食</h5>
+	</header>
+	<div class="aboutLink sticky-fade">いつか作るABOUTはここ</div>
+※文字削除後はスペースで空ける【51まで作成中】
+	<section id="wrap">
+		<ul class="path sticky-fade">
+			<li>Index</li>
+		</ul>
+
+		<div id="Container" class="worksList">
+			<a href="/works/202604_gankebeer/" onmouseover="handleMouseOver('img43', 'text43')" onmouseout="handleMouseOut()">
+				<ul class="category">
+					<li>VI</li>
+					<li>Logo</li>
+					<li>Package</li>
+					<li>Graphic</li>
+				</ul>
+				<div class="projectName">
+					<h4>GANKE BEER_ブランディング</h4>
+					<p>2026</p>
+				</div>
+			</a>
+			<a href="/works/202509_koyukai/" onmouseover="handleMouseOver('img41', 'text41')" onmouseout="handleMouseOut()">
+				<ul class="category">
+					<li>Logo</li>
+					<li>Tools</li>
+					<li>Web</li>
+					<li>Photo direction</li>
+					<li>Illust direction</li>
+				</ul>
+				<div class="projectName">
+					<h4>功有会_リブランディング</h4>
+					<p>2025</p>
+				</div>
+			</a>
+			<a href="/works/202503_jtcx/" onmouseover="handleMouseOver('img40', 'text40')" onmouseout="handleMouseOut()">
+				<ul class="category">
+					<li>Logo</li>
+					<li>KV</li>
+					<li>Tools</li>
+				</ul>
+				<div class="projectName">
+					<h4>Muture_JTCXコミュニティ</h4>
+					<p>2025</p>
+				</div>
+			</a>
+			<a href="/works/202502_muture/" onmouseover="handleMouseOver('img39', 'text39')" onmouseout="handleMouseOut()">
+				<ul class="category">
+					<li>Logo</li>
+					<li>KV</li>
+					<li>Web</li>
+					<li>Motion</li>
+					<li>Tools</li>
+				</ul>
+				<div class="projectName">
+					<h4>Muture_Muturelism Conference 2025</h4>
+					<p>2025</p>
+				</div>
+			</a>
+			<a href="https://pfq.jp/works/shachihata-100th/" onmouseover="handleMouseOver('img38', 'text38')" onmouseout="handleMouseOut()">
+				<ul class="category">
+					<li>Web</li>
+					<li>Motion</li>
+				</ul>
+				<div class="projectName">
+					<h4>シヤチハタ_100周年記念サイト</h4>
+					<p>2025</p>
+				</div>
+			</a>
+			<a href="https://pfq.jp/works/ikimono-tokyu/" onmouseover="handleMouseOver('img36', 'text36')" onmouseout="handleMouseOut()">
+				<ul class="category">
+					<li>Web</li>
+					<li>Direction</li>
+					<li>Photo direction</li>
+					<li>Motion</li>
+				</ul>
+				<div class="projectName">
+					<h4>いきもの東急不動産_巣箱プロジェクト</h4>
+					<p>2024</p>
+				</div>
+			</a>
+			<a href="https://pfq.jp/works/uniqlo-lucky-march/" onmouseover="handleMouseOver('img35', 'text35')" onmouseout="handleMouseOut()">
+				<ul class="category">
+					<li>Web</li>
+					<li>Direction</li>
+					<li>Illust Direction</li>
+				</ul>
+				<div class="projectName">
+					<h4>ユニクロ_感謝祭LUCKY MARCH</h4>
+					<p>2023</p>
+				</div>
+			</a>
+			<a href="https://pfq.jp/works/fujiya_cake/" onmouseover="handleMouseOver('img34', 'text34')" onmouseout="handleMouseOut()">
+				<ul class="category">
+					<li>Web</li>
+					<li>Direction</li>
+					<li>Photo direction</li>
+				</ul>
+				<div class="projectName">
+					<h4>不二家_ケーキ・洋菓子ブランドサイト</h4>
+					<p>2023</p>
+				</div>
+			</a>
+			<a href="https://pfq.jp/works/kajima-concrete-base/" onmouseover="handleMouseOver('img31', 'text31')" onmouseout="handleMouseOut()">
+				<ul class="category">
+					<li>Web</li>
+					<li>Direction</li>
+				</ul>
+				<div class="projectName">
+					<h4>鹿島建設_KAJIMA CONCRETE BASE</h4>
+					<p>2023</p>
+				</div>
+			</a>
+			<a href="https://pfq.jp/works/bandai-gashapon-45th/" onmouseover="handleMouseOver('img27', 'text27')" onmouseout="handleMouseOut()">
+				<ul class="category">
+					<li>Logo</li>
+					<li>UI</li>
+					<li>Graphic</li>
+				</ul>
+				<div class="projectName">
+					<h4>株式会社バンダイ「ガシャポン」45周年記念自販機開発</h4>
+					<p>2022</p>
+				</div>
+			</a>
+			<a href="https://pfq.jp/works/kajima-hirakata-ar/" onmouseover="handleMouseOver('img24', 'text24')" onmouseout="handleMouseOut()">
+				<ul class="category">
+					<li>APP</li>
+					<li>Logo</li>
+					<li>UI</li>
+					<li>Graphic</li>
+				</ul>
+				<div class="projectName">
+					<h4>鹿島建設_新名神高速道路「ヒラカタエアー」APP+LOGO</h4>
+					<p>2021</p>
+				</div>
+			</a>
+			<a href="https://pfq.jp/works/fishermanjapan/" onmouseover="handleMouseOver('img23', 'text23')" onmouseout="handleMouseOut()">
+				<ul class="category">
+					<li>Web</li>
+				</ul>
+				<div class="projectName">
+					<h4>フィッシャーマン・ジャパン_公式サイトリニューアル</h4>
+					<p>2021</p>
+				</div>
+			</a>
+			<a href="https://pfq.jp/works/kajima-dx-labo-ar/" onmouseover="handleMouseOver('img21', 'text21')" onmouseout="handleMouseOut()">
+				<ul class="category">
+					<li>APP</li>
+					<li>Logo</li>
+					<li>UI</li>
+					<li>Illustration</li>
+				</ul>
+				<div class="projectName">
+					<h4>鹿島建設_成瀬ダム「KAJIMA DX LABO」APP+LOGO</h4>
+					<p>2020</p>
+				</div>
+			</a>
+			<a href="https://pfq.jp/works/sakanadia-2/" onmouseover="handleMouseOver('img18', 'text18')" onmouseout="handleMouseOut()">
+				<ul class="category">
+					<li>Web</li>
+					<li>Logo</li>
+					<li>Motion</li>
+				</ul>
+				<div class="projectName">
+					<h4>JF全漁連_メディアサイト「Sakanadia」</h4>
+					<p>2019</p>
+				</div>
+			</a>
+			<a href="https://pfq.jp/works/moshikabe/" onmouseover="handleMouseOver('img14', 'text14')" onmouseout="handleMouseOut()">
+				<ul class="category">
+					<li>OOH</li>
+					<li>KV</li>
+					<li>Logo</li>
+					<li>Graphic</li>
+					<li>UI</li>
+				</ul>
+				<div class="projectName">
+					<h4>ピラミッドフィルムクアドラ_自社プロダクト「もし壁」</h4>
+					<p>2018</p>
+				</div>
+			</a>
+		</div>
+
+		<!-- フッター START -->
+		<script>footer();</script>
+	</section>
+
+	<div class="aboutBox">
+		<div class="aboutImage"><img id="def" src="/common/images/yoru.webp" alt="default image"></div>
+		<p id="textdef" class="aboutText">🌜</p>
+	</div>
+
+	<div class="textnone">
+	相互壁ドン”することでお互いへの気持ちの高まりが計測できるBOX型体験装置「もし、二人が壁ドンしたなら・・・二人の壁はなくなるのかもしれない。」略して『もし壁』を制作。筐体内で行われる“相互壁ドン”によって、どれだけドキドキしたかを手のひらの脈拍・発汗・温度のセンシングによって測定し、お互いのドキドキが一定値を超えることで、事前入力したメッセージが交換できるという仕組みの体験デバイス。電話ボックスサイズの筐体内部は完全な密室になっており、二人の距離はわずか十数cm！二人のドキドキを演出する「暗い」×「狭い」×「近い」という条件で設計された筐体内で、カップルや友達同士はもちろん、初対面の二人でも思わず“ドキッ”としてしまうことは間違いなし！
+	JF全漁連は、情報発信の強化により国民の漁業への理解醸成を図るため、Webメディア「Sakanadia」を立ち上げた。これまで発行していた季刊誌とは打って変わり、明るいトンマナで大衆に向けたデザインとUI設計に尽力し、JF全漁連の開かれた姿勢を体現。Sakanadiaロゴはアニメーションを付け、より多くのユーザーに愛着を持ってもらう一助となっている。サーバ面では、Webメディアということを考慮し、軽量化を目的としてオブジェクトストレージを採用。サイトの読み込み速度やコストの良化を提案し、一括で構築を行った。JF全漁連はこのSakanadiaから、より多くのユーザーに向けて楽しく正しい情報を発信し、多くの人が漁業を身近に感じ漁業への関心を深めることで、持続可能なニッポンの漁業を未来へつなげていくことを目指している。
+	鹿島建設の変⾰・デジタル化の旗印を目指して。「KAJIMADXLABO」のネーミングに合わせて、鹿島建設の変⾰・デジタル化の旗印になるよう先進感を感じられるデザインを⽬指した。研究室・ラボ・施設を想起させる直⽅体のイラストと、ラウンドを基調とした書体で親しみやすさ・先進感を印象付けるロゴとした。日本語のタグラインにも英字と同様の書体を用いて連動感を高めるとともに、曲線や抜きのあしらいがデザインのポイントとなっている。
+	フィッシャーマン・ジャパンのブランディング観を継承しながら。公式サイトながらブランドサイトの意味合いも強く、世界観と情報整理のバランスを考慮し考慮しながら、ソリッドな印象を持たせたデザインとした。カラーはモノトーンを使用。フィッシャーマン・ジャパンのブランディング観を継承している。黒背景を用いることでKVや画像が映え、プロジェクトをより注目させている。UI設計としては、KVやコピーで団体イメージを伝えてからカテゴリー別プロジェクトとし、新規ユーザーに伝わりやすい構成に。プロジェクトページでは4カテゴリーのソート機能をつけ、ユーザーが得たい情報に辿り着きやすいように設計した。
+	アートディレクションの方向性は、技術の高さや先進性をシンプルなモノトーンで訴求し、視認性の高い要素を取り入れることでユーザビリティを考慮しながらも革新的かつ安定感のある世界観とした。UIデザインは、天候や環境に左右される閲覧場所であることから「フォントサイズや要素の大きさにより視認性の高さと、赤を用いて誘目性を高めるとともに“企業らしさ”にもつなげる」、初見のユーザーでも扱いやすい「レイアウト・補足要素・演出面から操作性の高い易しいUIに」という2点をポイントとしたデザインに。
+	王道感と先進性を併せ持ち、ぱっと目につき「体験してみたい」と思わせるデザインに。ロゴデザインは、「未来のガシャポン」「ODYSSEYというネーミングの響きからイメージできる壮大さ」「アルファベットを用いて王道感がありつつも先進性を感じるデザイン」の3点をキーワードに制作。伝統的かつ重厚感のあるタイポグラフィを用いながら先進性の印象も持たせることで、「45周年の歴史と未来＝長い冒険旅行」を表現している。視差効果が特徴となる「未来のガシャポン」の筐体の形状を「Y」の字に取り入れ、世界観の連動性を高めた。
+	持続可能な未来を、コンクリートとともに業界関係者をターゲットとし、CASESTUDYを中心にコンクリートの各技術を紹介している。キービジュアルでは「環境に配慮したコンクリート」というイメージを訴求し、CASESTUDY/TECHNOLOGYエリアは球体を用いた洗練された世界観で、未来の進化したコンクリートを印象付けている。
+	不二家の想いや美味しさの追求を認知させ、「食べたい」のサイクルを作る。情緒的価値は、VI改訂を入り口に、美味しさがアップデートされた「商品が主役のサイト」に。ケーキや原材料の写真素材を活かしながら、シンプルでVIと商品が映える世界観に。機能的価値は、100年続く企業である不二家の「美味しさの追求」や「安心安全」といった取り組みを記事化し発信していく。アップデートする企業の姿勢と商品を紐付けることで、より不二家の魅力を認知させる。
+	アバターをコーディネートして冬本番への行進に参加、クーポンが当たる2023年秋冬ユニクロ感謝祭デジタル施策「感謝祭LUCKYMARCH」を制作。「すすめ、冬本番へ。」基本ボディを選択しユニクロの冬アイテムでコーディネートすると、自分だけのアバターが完成！作成したアバターで行進に参加すると、たくさんのアバターと共に秋から冬へ変化しながら様々なシーンを歩いていく。最後にはラッキーなことが起こるかも！？といった秋から冬へと移り変わる季節にぴったりの「感謝祭LUCKYMARCH」サイト。「『そうだ冬支度しよう！』と思ってもらうために、冬支度（=冬に向かう準備）の“ワクワク”をつくる。」をコンセプトに、思わず温かいアイテムが欲しくなる、コーディネートして出かけたくなる、冬への盛り上がりを“行進”や“シーン”を用いて一つのストーリーのように設計・表現した。
+	人だけじゃなく、鳥や昆虫たちにとってもいい不動産会社になることを目指すプロジェクト。人間の快適さが中心になりがちな都会だが、その隣には様々な生き物が暮らしている。東急不動産は、人だけじゃなく、鳥や昆虫たちにとってもいい不動産会社になることを目標に掲げ、「いきもの東急不動産」を立ち上げた。例えば、東急プラザ表参道原宿の屋上は、いきものにとって代々木公園や明治神宮といった緑豊かな緑地と緑地をつなぐ中継地点。そこに来るシジュウカラや蝶なども小さなお客様と捉え、「シジュウカラの邸宅」と名付けた独自設計の物件(巣箱)を3棟開発・設置。プロジェクトサイトを立ち上げ、人間向けの物件ページのような建て付けで紹介するコンテンツを制作した。また、この取り組みが拡大するよう、各物件の設計図をオープンソース化し、サイトからダウンロードできるようにした。
+	シヤチハタは100年間、時代に合わせて形式は変われど、常に「しるしの価値」を提供し続けてきた。そんなブランドのアイデンティティと呼べる「しるし」を赤い丸の「印影」としてシンボリックに表現。周年を祝う行進のようなモーションでシンプルな要素ながら賑やかな画面に。画面内の「しるし」は全てがコンテンツの入り口となっており、赤い丸は「祝辞」、青い四角は「歴史」、青い三角は100年のストーリーを印影で表現した「スタンプムービー」、青い三本線は「100周年記念商品」にそれぞれ遷移する。また、時折現れる「鯱旗」の印影をクリックすると特別なプレゼントが当たる隠しコンテンツも実装した。
+	思いがけない出会いを通じて、新しい問いが生まれ、私たちの人生やビジネスに転機をもたらす瞬間となる。グッドパッチと丸井グループのジョイントベンチャーである株式会社Mutureが主催する「MuturelismConference2025」カンファレンスイベントのクリエイティブを担当。日本を代表する大企業とソーシャルセクターの組織変革・DX推進を事業内容とし、「相利共生の未来を実現する」というビジョンのもとに、ビジネスにおける二項対立や情報の非対称性をなくし、すべての個性が尊重され、多彩な価値観が共生する仕組みのデザインを目指されている。そんな企業が新しいカンファレンスを立ち上げた。偶発的な出会いから新しい問いを見つける、知と実の交差点といったキーワードを元に、交差点、オブジェクト、文字たちが重なりあい相乗効果をもたらしていく様を表現している。DXという枠に捉われずに、関係作りをするためのヒントを持ち寄り、共有し合う、人が主体なイベントとなっている。
+	大企業が変われば日本が変わる！JTC・エンタープライズ組織で日々奮闘しながら、様々な取り組みをしている方々のユースケースを学び合い内部での努力を共通の知にしていきたいという思いから生まれた勉強会コミュニティ。大企業の変革という同じ志を持ち、日本を前進させる当事者同士で学び合い交流を深め、良い学びの場となることを目指している。
+	SHIFTBRAIN社の旗振りのもと、社会福祉法人 功有会のブランディングをトータルで支援。ワークショップを交えたMVVの再定義から、ロゴ・名刺・Webサイト・写真・イラストまで一貫制作。「あなたのよりどころ。」のメッセージを核に、写真とイラスト２つのビジュアルで功有会のあり方が正しく表現できるようにクリエイティブをつくりあげた。奈良県下18施設・18サービスの広がりと、高齢・障がい・児童まで網羅する支援の姿勢を温かく誠実に伝えている。広報・採用の双方で活用可能なブランド基盤となっている。夜.はアートディレクション・デザインを担当しました。
+	</div>
+
+</div>
+<!--/container-->
+</body>
+</html>
+
